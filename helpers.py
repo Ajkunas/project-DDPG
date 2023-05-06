6cb76ec8f9681bf4668a498e6cfb635319636e01
Kate <trimbachkatya@gmail.com>
2023-05-06 14:16:14 -0700

    added Q network

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import torch 
 
 class NormalizedEnv(gym.ActionWrapper):
     """ Wrap action """
@@ -7,11 +8,15 @@ class NormalizedEnv(gym.ActionWrapper):
     def action(self, action):
         act_k = (self.action_space.high - self.action_space.low)/ 2.
         act_b = (self.action_space.high + self.action_space.low)/ 2.
+        
+        # added to be supported by torch.
         return act_k * action + act_b
 
     def reverse_action(self, action):
         act_k_inv = 2./(self.action_space.high - self.action_space.low)
         act_b = (self.action_space.high + self.action_space.low)/ 2.
+        
+        
         return act_k_inv * (action - act_b)
     
     
